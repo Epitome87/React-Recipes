db5a40d5371984553aaf3ec70a90c3a22d34f753
Matthew McGrath <rubywep@hotmail.com>
2022-03-24 11:38:47 -0700

    Begin lazy loading of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,31 @@
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
-import Navigation from './Components/Layout/Navigation';
-import {
-  Categories,
-  ErrorPage,
-  Landing,
-  Recipe,
-  Recipes,
-  Search,
-  Test,
-  UserProfile,
-} from './Pages';
+const Navigation = lazy(() => import('./Components/Layout/Navigation'));
+const Landing = lazy(() => import('./Pages/Home'));
+const Search = lazy(() => import('./Pages/Search'));
+const Recipe = lazy(() => import('./Pages/Recipes'));
+const Recipes = lazy(() => import('./Pages/Recipe'));
+const UserProfile = lazy(() => import('./Pages/UserProfile'));
+const Error = lazy(() => import('./Pages/Error'));
 
 function App() {
   return (
-    <Box maxW='1200px' bg='#28282B' h='100%' mx='auto'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/search/:query' element={<Search />} />
-          <Route path='/recipes' element={<Recipes />} />
-          <Route path='/recipes/:recipeId' element={<Recipe />} />
-          <Route path='/users/:userId' element={<UserProfile />} />
-          <Route path='/test' element={<Test />} />
-          <Route path='*' element={<ErrorPage />} />
-        </Routes>
-        <Navigation />
-      </Router>
+    <Box maxW="1200px" bg="#28282B" h="100%" mx="auto">
+      <Suspense fallback={<React.Fragment>Loading...</React.Fragment>}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/search/:query" element={<Search />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipes/:recipeId" element={<Recipe />} />
+            <Route path="/users/:userId" element={<UserProfile />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Navigation />
+        </Router>
+      </Suspense>
     </Box>
   );
 }

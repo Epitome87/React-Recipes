77213e238ec3750be1a97cc3fcc58524348344a8
Matthew McGrath <rubywep@hotmail.com>
2022-04-03 18:56:53 -0700

    Add UserProvider. Lazy load Page-level components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,42 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
+import { UserContext, UserProvider } from './Contexts/UserContext';
 const Navigation = lazy(() => import('./Components/Layout/Navigation'));
 const Landing = lazy(() => import('./Pages/Home'));
 const Search = lazy(() => import('./Pages/Search'));
-const Recipe = lazy(() => import('./Pages/Recipes'));
-const Recipes = lazy(() => import('./Pages/Recipe'));
+const Recipe = lazy(() => import('./Pages/Recipe'));
+const Recipes = lazy(() => import('./Pages/Recipes'));
 const UserProfile = lazy(() => import('./Pages/UserProfile'));
 const Error = lazy(() => import('./Pages/Error'));
+// import Navigation from './Components/Layout/Navigation';
+// import Landing from './Pages/Home';
+// import Search from './Pages/Search';
+// import Recipe from './Pages/Recipe';
+// import Recipes from './Pages/Recipes';
+// import UserProfile from './Pages/UserProfile';
+// import Error from './Pages/Error';
 
 function App() {
   return (
-    <Box maxW="1200px" bg="#28282B" h="100%" mx="auto">
-      <Suspense fallback={<React.Fragment>Loading...</React.Fragment>}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/search/:query" element={<Search />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/recipes/:recipeId" element={<Recipe />} />
-            <Route path="/users/:userId" element={<UserProfile />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-          <Navigation />
-        </Router>
-      </Suspense>
-    </Box>
+    <UserProvider>
+      <Box maxW="1200px" bg="#28282B" h="100%" mx="auto">
+        <Suspense fallback={<React.Fragment>Loading...</React.Fragment>}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/search/:query" element={<Search />} />
+              <Route path="/recipes" element={<Recipes />} />
+              <Route path="/recipes/:recipeId" element={<Recipe />} />
+              <Route path="/users/:userId" element={<UserProfile />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+            <Navigation />
+          </Router>
+        </Suspense>
+      </Box>
+    </UserProvider>
   );
 }
 

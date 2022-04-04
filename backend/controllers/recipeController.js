e413d75c14cbe74bf70e5a6801fe816896e7670c
Matthew McGrath <rubywep@hotmail.com>
2022-04-03 18:34:16 -0700

    Develop Controllers a little more

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,6 +1,16 @@
 const Recipe = require('../models/recipeModel');
 const asyncHandler = require('express-async-handler');
 
+const getPersonalRecipes = asyncHandler(async (req, res) => {
+  // One way to get recipes by this user
+  const recipesByUser = await Recipe.find({ author: req.user._id });
+  // Another way:
+  // await req.user.populate('recipes');
+
+  res.status(200).json(recipesByUser);
+  // res.status(200).json(req.user.recipes);
+});
+
 // @desc Get Recipes
 // @route GET /api/recipes
 // @access Private
@@ -14,20 +24,35 @@ const getRecipes = asyncHandler(async (req, res) => {
 // @access Private
 const getRecipe = asyncHandler(async (req, res) => {
   const recipe = await Recipe.findById(req.params.id);
+
+  // This works!!! But we probably don't want to expose all the author's info here!
+  await recipe.populate('author');
+  console.log('AUTHOR', recipe.author);
+
   res.status(200).json(recipe);
 });
 
 // @desc Set recipe
 // @route POST /api/recipes
 // @access Private
-const setRecipe = asyncHandler(async (req, res) => {
+const createRecipe = asyncHandler(async (req, res) => {
+  console.log('CREATING RECIPE');
   if (!req.body) {
     res.status(400);
     throw new Error('Please provide appropriate recipe information');
   }
 
-  const { name, image, timeToPrep, difficulty, rating } = req.body;
-  const recipe = await Recipe.create({ name, image, timeToPrep, difficulty, rating });
+  const { title, image, timeToPrep, difficulty, rating } = req.body;
+  const author = req.user._id;
+
+  const recipe = await Recipe.create({
+    title,
+    image,
+    timeToPrep,
+    difficulty,
+    rating: 3,
+    author,
+  });
   res.status(200).json(recipe);
 });
 
@@ -36,11 +61,20 @@ const setRecipe = asyncHandler(async (req, res) => {
 // @access Private
 const updateRecipe = asyncHandler(async (req, res) => {
   const { id: recipeId } = req.params;
+
   const recipe = await Recipe.findById(recipeId);
 
+  // No recipe with this ID
   if (!recipe) {
     res.status(400);
     throw new Error('Recipe not found');
+  }
+
+  // Does this recipe belong to someone?
+  if (recipe.author && recipe.author !== req.user_id) {
+    // If this recipe has an author, and it is not this user, do not allow it to be PATCHed
+    res.status(400);
+    throw new Error('You do not have permission to edit this Recipe');
   }
 
   // Pass in options with key of "new" set to true -- so we create the document if it does not exist
@@ -70,9 +104,10 @@ const deleteRecipe = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
+  getPersonalRecipes,
   getRecipes,
   getRecipe,
-  setRecipe,
+  createRecipe,
   updateRecipe,
   deleteRecipe,
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,31 +12,87 @@ const getUsers = asyncHandler(async (req, res) => {
 // @desc Get User
 // @route GET /api/users/:id
 // @access Private
+// const getUser = asyncHandler(async (req, res) => {
+//   // const user = await User.findById(req.params.id).populate('favoriteMeals');
+//   // User must already be found via our Auth middleware if we reach this point
+//   console.log('GTEETTING USER');
+//   res.status(200).json({
+//     user: req.user,
+//     favoriteMeals: await req.user.populate('favoriteMeals'),
+//   });
+// });
+// TODO:
 const getUser = asyncHandler(async (req, res) => {
+  console.log(req.params.id);
   const user = await User.findById(req.params.id).populate('favoriteMeals');
-  res.status(200).json(user);
+  // User must already be found via our Auth middleware if we reach this point
+  console.log('GTEETTING USER', user);
+  // res.status(200).json({
+  //   user: req.user,
+  //   favoriteMeals: await req.user.populate('favoriteMeals'),
+  // });
+  res.status(200).json({
+    user,
+    // favoriteMeals: await user.populate('favoriteMeals'),
+  });
+});
+
+// TODO:
+const updateFavoriteMeals = asyncHandler(async (req, res) => {
+  // Body will have isFavorite boolean and mealID set to the ObjectID of the meal in question
+  const { isFavorite, mealID } = req.body;
+  console.log(
+    `Updating User ${req.params.id} for meal ${mealID} setting it to Favorite=${isFavorite}`
+  );
+
+  const user = await User.findById(req.params.id);
+
+  // If this meal isn't a Favorite, make sure it's not in favoriteMeals list
+  user.favoriteMeals.forEach((meal) => console.log('ID', meal._id.toString()));
+  if (!isFavorite) {
+    user.favoriteMeals = user.favoriteMeals.filter(
+      (meal) => meal._id.toString() !== mealID
+    );
+
+    await user.save();
+    return res.status(200).json({ user });
+  }
+
+  // But if the Meal is to be a Favorite, add it!
+  if (user.favoriteMeals.some((meal) => meal._id.toString() === mealID)) {
+    // Meal is already in favorite list!
+    return res.status(200).json({ user });
+  } else {
+    user.favoriteMeals.push(mealID);
+    await user.save();
+    res.status(200).json({ user });
+  }
 });
 
 // @desc Set user
 // @route POST /api/users
 // @access Private
-const setUser = asyncHandler(async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   if (!req.body) {
     res.status(400);
     throw new Error('Please provide appropriate user information');
   }
 
-  const { name, image, favoriteMeals } = req.body;
-  const user = await User.create({ name, image, favoriteMeals });
-  res.status(200).json(user);
+  const { email, password, name, image, favoriteMeals } = req.body;
+  const user = new User({ email, password, name, image, favoriteMeals });
+  await user.save();
+
+  const token = await user.generateAuthToken();
+
+  res.status(201).json({ user, token });
 });
 
 // @desc Update User
 // @route PUT /api/users/:id
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
-  const { id: userId } = req.params;
-  const user = await User.findById(userId);
+  // User can only update himself, so we don't need to specify an ID -- we grab the currently auth'd user's ID
+  const user = await User.findById(req.user._id);
 
   if (!user) {
     res.status(400);
@@ -44,7 +100,7 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 
   // Pass in options with key of "new" set to true -- so we create the document if it does not exist
-  const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, req.body, {
     new: true,
   });
 
@@ -52,12 +108,11 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 // @desc Delete User
-// @route DELETE /api/users/:id
+// @route DELETE /api/users/me
 // @access Private
 const deleteUser = asyncHandler(async (req, res) => {
-  const { id: userId } = req.params;
-
-  const user = await User.findById(userId);
+  // User can only delete himself, so we don't need to specify an ID -- we grab the currently auth'd user's ID
+  const user = await User.findById(req.user._id);
 
   if (!user) {
     res.status(400);
@@ -66,13 +121,42 @@ const deleteUser = asyncHandler(async (req, res) => {
 
   await user.remove();
 
-  res.status(200).json({ id: userId });
+  res.status(200).json({ id: req.user._id });
 });
 
+// Log the user in given their email / password
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findByCredentials(email, password);
+  const token = await user.generateAuthToken();
+
+  res.status(200).send({ user, token });
+};
+
+// Logout by clearing the User's current Token
+const logout = async (req, res) => {
+  req.user.tokens = req.user.tokens.filter((token) => token !== req.token);
+  await req.user.save();
+  res.status(200).send();
+};
+
+// Logout by clearing all of the User's Tokens
+const logoutAll = async (req, res) => {
+  req.user.tokens = [];
+
+  await req.user.save();
+  res.status(200).send();
+};
+
 module.exports = {
+  updateFavoriteMeals,
   getUsers,
   getUser,
-  setUser,
+  createUser,
   updateUser,
   deleteUser,
+  login,
+  logout,
+  logoutAll,
 };

cb6ddf69ad4e932bf91b0601dcf979d2ebe398e7
Matthew McGrath <rubywep@hotmail.com>
2022-04-03 18:33:24 -0700

    Add some more routes

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth.js');
 
 const {
-    getRecipes,
-    getRecipe,
-    setRecipe,
-    updateRecipe,
-    deleteRecipe,
-  } = require('../controllers/recipeController');
-  
-  // GET all Recipes and Get and Create a Recipe
-  router.route('/').get(getRecipes).post(setRecipe);
-  
-  // Get, Update and Delete a Recipe by ID
-  router.route('/:id').get(getRecipe).delete(deleteRecipe).put(updateRecipe);
+  getPersonalRecipes,
+  getRecipes,
+  getRecipe,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+} = require('../controllers/recipeController');
+
+// GET all Recipes and Get and Create a Recipe
+router.route('/').get(getRecipes).post(auth, createRecipe);
+
+router.route('/my').get(auth, getPersonalRecipes);
+
+// Get, Update and Delete a Recipe by ID
+router
+  .route('/:id')
+  .get(getRecipe)
+  .delete(auth, deleteRecipe)
+  .patch(auth, updateRecipe);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const auth = require('../middleware/auth.js');
 const router = express.Router();
 const {
-  getUsers,
+  updateFavoriteMeals,
   getUser,
-  setUser,
+  createUser,
   updateUser,
   deleteUser,
+  login,
+  logout,
+  logoutAll,
 } = require('../controllers/userController');
 
 // GET all Users and Get and Create a User
-router.route('/').get(getUsers).post(setUser);
+router.route('/').post(createUser);
 
-// Get, Update and Delete a User by ID
-router.route('/:id').get(getUser).delete(deleteUser).put(updateUser);
+// Get, Update and Delete a User (self)
+router
+  .route('/me')
+  .get(auth, getUser)
+  .delete(auth, deleteUser)
+  .patch(auth, updateUser);
+
+// Routes for logging in and out
+router
+  .patch('/:id', updateFavoriteMeals)
+  .get('/:id', getUser) // TODO: Remove this eventually
+  .post('/login', auth, login)
+  .post('/logout', auth, logout)
+  .post('/logoutAll', auth, logoutAll);
 
 module.exports = router;
